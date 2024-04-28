7fdd4d18ba46e1b217b06173e3a12d5fab0709f6
jacagift <166239691+jacagift@users.noreply.github.com>
2024-04-28 11:59:09 -0300

    Update api.py

--- api.py
+++ api.py
@@ -186,7 +186,7 @@ def definir_tipo_cartao(card):
 def checker(card, month, year, cvv):
     try:
         time.sleep(10)
-        p = {'https': 'http://brd-customer-hl_b12cf4ef-zone-rdpremium-country-us:qj77tznsi49h@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-rdpremium-country-us:qj77tznsi49h@brd.superproxy.io:22225'}
+        p = {'https': 'http://brd-customer-hl_b12cf4ef-zone-privado-country-us:6f2jb118cxl2@brd.superproxy.io:22225', 'http':'http://brd-customer-hl_b12cf4ef-zone-privado-country-us:6f2jb118cxl2@brd.superproxy.io:22225'}
         start_time = time.time() 
 
 
